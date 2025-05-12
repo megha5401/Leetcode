50178b6ede3f50fba27b2b0828d967f65d89673f
megha5401 <165200185+megha5401@users.noreply.github.com>
2025-05-12 22:42:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (97.17%) - LeetSync

---
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int ans = 1;
+        for (int num : nums) {
+            if (num == 0) return 0;
+            if (num < 0) ans *= -1;
+        }
+        return ans;
+    }
+}
