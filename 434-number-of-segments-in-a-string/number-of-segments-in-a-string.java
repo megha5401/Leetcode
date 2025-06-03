0babcc985dd33175b991a9eda8e5fb63469220f8
megha5401 <165200185+megha5401@users.noreply.github.com>
2025-06-03 23:05:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (78.00%) - LeetSync

--- 434-number-of-segments-in-a-string/number-of-segments-in-a-string.java
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.java
@@ -1,8 +1,12 @@
 class Solution {
     //megha0
     public int countSegments(String s) {
-        if (s == null || s.trim().isEmpty()) return 0;
-        String a [] = s.trim().split("\\s+");
-        return a.length;
+        int a = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if ((i == 0 || s.charAt(i-1) == ' ') && s.charAt(i) != ' ') {
+                a++;
+            }
+        }
+        return a;
     }
 }
