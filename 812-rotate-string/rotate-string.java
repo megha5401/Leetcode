38ddc9aba1c363cdc5f58881624e9a74629091a0
megha5401 <165200185+megha5401@users.noreply.github.com>
2025-06-08 22:49:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (90.22%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,9 @@
+class Solution {
+    //megha0
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        return (s + s).contains(goal);
+    }
+}
